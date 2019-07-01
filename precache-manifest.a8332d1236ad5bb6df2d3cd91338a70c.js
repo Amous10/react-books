b982117ee9998d56ca5fcbe4ab5eee3d863cc307
Amy Merrill <amous.fullstack@gmail.com>
2019-07-01 10:55:11 -0700

    Updates

---
+++ precache-manifest.a8332d1236ad5bb6df2d3cd91338a70c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c03a91cb7976545d035c87b145b6cee",
+    "url": "/react-books/index.html"
+  },
+  {
+    "revision": "12bbb9ae401f7c6822c1",
+    "url": "/react-books/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-books/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "12bbb9ae401f7c6822c1",
+    "url": "/react-books/static/js/main.04a4016c.chunk.js"
+  },
+  {
+    "revision": "a7b9751a9538460bc663",
+    "url": "/react-books/static/js/runtime~main.deca0de0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-books/static/media/logo.5d5d9eef.svg"
+  }
+]);
